789dd7d5668c57fd21448852fb3f3b9e55a3c036
Hector Garcia <hg425823@gmail.com>
2024-09-10 10:14:10 -0600

    Update HomeController

--- KM.SysControlAdmin.WebApp/Controllers/HomeController.cs
+++ KM.SysControlAdmin.WebApp/Controllers/HomeController.cs
@@ -1,32 +1,29 @@
+#region REFERENCIAS
+/// Referencias Necesarias Para El Correcto Funcionamiento
 using KM.SysControlAdmin.WebApp.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
+
+#endregion
+
 namespace KM.SysControlAdmin.WebApp.Controllers
 {
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [Authorize(Roles = "Desarrollador")]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [Authorize(Roles = "Desarrollador")]
+        public IActionResult Dashboard()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
