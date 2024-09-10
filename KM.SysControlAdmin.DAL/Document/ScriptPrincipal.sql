0c83d7090a550c393c4b978f7697ecd8afa6059a
Hector Garcia <hg425823@gmail.com>
2024-09-10 10:56:25 -0600

    Creacion e integracion de archivo sql de respaldo

---
+++ KM.SysControlAdmin.DAL/Document/ScriptPrincipal.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE KMSysControlAdminDB
+GO
+    USE KMSysControlAdminDB
+GO
+CREATE TABLE [Role](
+    Id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [Name] VARCHAR(30) NOT NULL
+    );
+GO
+CREATE TABLE [User](
+    Id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [Name] VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    [Password] VARCHAR(100) NOT NULL,
+    [Status] TINYINT NOT NULL,
+    DateCreated DATETIME NOT NULL,
+	ImageData VARBINARY(MAX) NULL,
+	IdRole INT NOT NULL FOREIGN KEY REFERENCES [Role](Id)
+    );
+GO
+	INSERT INTO [Role] VALUES('Desarrollador');
+GO
+    INSERT INTO [User] (IdRole, [Name], LastName, Email, [Password], [Status], DateCreated) 
+    VALUES (1, 'Flexcode', 'Freelancer', 'DesAdmin@kerigmamusic.com', 'c8aa131427a72781b156ac723ddb917f', 1, SYSDATETIME());
+GO
