986eca564c35911019c00eddf7b77d06a4d4c2e6
Hector Garcia <hg425823@gmail.com>
2024-09-20 21:33:09 -0600

    Creacion de la Entidad Schedule

---
+++ KM.SysControlAdmin.EN/Schedule - EN/Schedule.cs
@@ -0,0 +1,34 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using System.ComponentModel.DataAnnotations;
+
+
+#endregion
+
+namespace KM.SysControlAdmin.EN.Schedule___EN
+{
+    public class Schedule
+    {
+        #region ATRIBUTOS DE LA ENTIDAD
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La hora de inicio es requerida")]
+        [Display(Name = "Hora de Inicio")]
+        [DataType(DataType.Time)]
+        public TimeSpan StartTime { get; set; }
+
+        [Required(ErrorMessage = "La hora de fin es requerida")]
+        [Display(Name = "Hora de Finalización")]
+        [DataType(DataType.Time)]
+        public TimeSpan EndTime { get; set; }
+        #endregion
+
+        //public List<Course> Course { get; set; } = new List<Course>(); // Propiedad de navegacion
+    }
+}
